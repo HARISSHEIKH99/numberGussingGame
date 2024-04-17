59cde5ff1712dc6423ec75d71e7ec7e3b047b08a
AyeshaHaris <ayeshaharis1999@gmail.com>
2024-04-17 15:45:27 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+let afterRamadan = "Eid";
+console.log(afterRamadan == "Eid");
+let AC = "coling";
+console.log(AC == "coling");
+let favoriteDish = "biryani";
+console.log(favoriteDish != "korma");
+let Age = 25;
+console.log(Age > 25);
+console.log(Age >= 25);
+let months = 12;
+console.log(months < 12);
+console.log(months <= 12);
+//modulus
+let num1 = 5; //denominater
+let num2 = 17; //nominater
+console.log(num1 % num2); //rimender 5
+export {};
